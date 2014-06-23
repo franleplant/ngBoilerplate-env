6a5f5915700f473c1067c840ca8593f1c48a56e1
franleplant <franleplant@gmail.com>
2014-06-23 15:03:12 -0300

    initial commit

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 apt-get install -y software-properties-common python-software-properties
 add-apt-repository -y ppa:chris-lea/node.js
+add-apt-repository -y ppa:git-core/ppa
 apt-get update
-apt-get install -y python-software-properties python g++ make nodejs samba vim
+apt-get install -y python-software-properties python g++ make nodejs samba vim git
 gem update --system
 gem install sass compass
